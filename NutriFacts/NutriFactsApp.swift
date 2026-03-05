7dfa34eb803f7551b9abc0aa5cc767610d8792a0
Prajakta Kulkarni <prachee.j@gmail.com>
2026-03-05 12:57:55 +0100

    Initial Commit

---
+++ NutriFacts/NutriFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriFactsApp.swift
+//  NutriFacts
+//
+//  Created by Prajakta Kulkarni on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct NutriFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NutriFactsTests/NutriFactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutriFactsTests.swift
+//  NutriFactsTests
+//
+//  Created by Prajakta Kulkarni on 05/03/2026.
+//
+
+import Testing
+@testable import NutriFacts
+
+struct NutriFactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
